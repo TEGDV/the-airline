e49bd3c1adee5a4aac0ca00c1322ec784e2f873b
TEGDV <apjair97@gmail.com>
2021-06-23 10:53:19 -0500

    :tada: Updated React Version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Panel from "./Panel";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <React.Fragment>
+            <div className="jumbotron">
+                <h4 className="display-4">Welcome to the Airline!</h4>
+            </div>
+
+            <div className="row">
+                <div className="col-sm">
+                    <Panel title="Balance">
+
+                    </Panel>
+                </div>
+                <div className="col-sm">
+                    <Panel title="Loyalty points - refundable ether">
+
+                    </Panel>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm">
+                    <Panel title="Available flights">
+
+
+                    </Panel>
+                </div>
+                <div className="col-sm">
+                    <Panel title="Your flights">
+
+                    </Panel>
+                </div>
+            </div>
+        </React.Fragment>
+    }
 }
-
-export default App;
