212bfb050096d4f0bc5d4259095cf0f765a91b75
TEGDV <apjair97@gmail.com>
2021-06-24 09:18:28 -0500

    :wrench: Config truffle to handdle solc ^0.8.0 compiler

--- truffle-config.js
+++ truffle-config.js
@@ -6,5 +6,17 @@ module.exports = {
       network_id: '*',
       gas: 5000000
     }
+  },
+    compilers: {
+    solc: {
+      version: "^0.8.0", // A version or constraint - Ex. "^0.5.0"
+      parser: "solcjs", // Leverages solc-js purely for speedy parsing
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200, // Optimize for how many times you intend to run the code
+        },
+      },
+    },
   }
 }
