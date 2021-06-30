f79e5fc916c236b74d5f7ece87bc5f66ff5560e3
TEGDV <apjair97@gmail.com>
2021-06-29 22:46:54 -0500

    :bug: Solved RPC JSON error on Buy flight

--- src/AirlineService.js
+++ src/AirlineService.js
@@ -6,6 +6,10 @@ const AirlineService = () => {
   const { web3, account, contract } = useContext(AirlineContext)
   const [flights, setFlights] = useState([])
 
+  const buyFlight = async (flightIndex, _value) => {
+    await contract.methods.buyFlight(flightIndex).send({ from: account, value: _value })
+  }
+
   useEffect(async () => {
     const totalFlights = (await contract.methods.totalFlights().call({ from: account }))
     const _flights = []
@@ -16,7 +20,7 @@ const AirlineService = () => {
     setFlights(_flights)
   }, [])
 
-  return (flights.map((flight, i) => <div key={i}>{flight.name} | {web3.utils.fromWei(flight.price, 'ether')}</div>)
+  return (flights.map((flight, i) => <div key={i}>{flight.name} | {web3.utils.fromWei(flight.price, 'ether')} <button onClick={() => buyFlight(i, flight.price)}>Buy it</button></div>)
   )
 }
 
