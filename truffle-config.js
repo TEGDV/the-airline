667294f045159b2013a32b42778cdfeb612cc0c3
TEGDV <apjair97@gmail.com>
2021-07-01 13:09:15 -0500

    :wrench: Added rinkeby deployment config

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,24 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const mnemonic = 'ten diesel combine spirit where six robust vicious pave gift budget bubble'
+const rinkebyEndPoint = 'https://rinkeby.infura.io/v3/6af9077f8a2048acadbef342a747f5b1'
 module.exports = {
-  contracts_build_directory: "./src/abis/",
+  contracts_build_directory: './src/abis/',
   networks: {
-    development: {      
+    development: {
       host: 'localhost',
       port: 7545,
       network_id: '*',
       gas: 5000000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, rinkebyEndPoint),
+      network_id: 4,
     }
   },
-    compilers: {
+  compilers: {
     solc: {
-      version: "^0.8.0", // A version or constraint - Ex. "^0.5.0"
-      parser: "solcjs", // Leverages solc-js purely for speedy parsing
+      version: '^0.8.0', // A version or constraint - Ex. "^0.5.0"
+      parser: 'solcjs', // Leverages solc-js purely for speedy parsing
       settings: {
         optimizer: {
           enabled: true,
